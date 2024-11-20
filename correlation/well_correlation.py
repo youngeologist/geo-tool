d41a106b67a560e5b9fabcb901a5dfa8dd54752e
youngeologist <asepher@gmail.com>
2024-11-20 13:58:25 +0800

    Update well_correlation.py

--- correlation/well_correlation.py
+++ correlation/well_correlation.py
@@ -125,7 +125,8 @@ def main():
 
     flatmarker = st.sidebar.selectbox("Flat log on marker", list(merged_marker['MARKER']))
     FillGR = st.sidebar.selectbox("Fill GR below Shale Base Line?", list(['NO','YES']))
-    ShaleBaseLine = st.sidebar.text_input("Shale Base Line", ShBaseLine)
+    # ShaleBaseLine = st.sidebar.text_input("Shale Base Line", ShBaseLine)
+    ShaleBaseLine = st.sidebar.slider('Shale Base Line', min_value=0, max_value=150, value=75)
     ShaleBaseLine = int(ShaleBaseLine)
 
     Depth_cm = (maxdepth - mindepth)*100
